1825b8bd6def93b0642ed0889eb5eaca731ef215
anjika17 <80506928+anjika17@users.noreply.github.com>
2022-05-28 09:40:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,32 @@ function gotResult(error, results) {
     }
     else{
         console.log(results);
+
+        random_number_R = Math.floor(Math.random()*255)+1 ;
+  random_number_G = Math.floor(Math.random()*255)+1 ;
+  random_number_B = Math.floor(Math.random()*255)+1 ;
+
+ document.getElementById("result_name").innerHTML = "I can hear : "+results[0].label ;
+  document.getElementById("result_confidence").innerHTML = "Accuracy : "+(results[0].confidence*100).toFixed(1)+"%" ;
+
+  
+  document.getElementById("result_name").style.color="rgb("+random_number_R+" , "+random_number_G+" , "+random_number_B+")" ;
+  document.getElementById("result_confidence").style.color="rgb("+random_number_R+" , "+random_number_G+" , "+random_number_B+")" ;
+
+  image=document.getElementById("Image") ;
+  if(results[0].label == "Background Noise"){
+image.src='background.jfif' ;
     }
-}
+  else if(results[0].label == "Barking"){
+    image.src='dog.jpg' ;
+  } 
+  else if(results[0].label == "Meowing"){
+      image.src='cat.jpg' ;
+  }
+  else if(result[0].label == "Chirping"){
+    image.src='bird.jfif' ;
+  }
+  else{
+    image.src='cow.png' ;
+  }
+}}
